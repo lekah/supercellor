cd22a8e2ce819dc2d17ac75bd01854c4d5f49aa0
Leonid Kahle <leonid.kahle@epfl.ch>
2018-08-13 17:12:15 +0200

    Added proper standardization to supercell generation, and standard test:

--- supercellor/supercell.py
+++ supercellor/supercell.py
@@ -13,21 +13,42 @@ except ImportError:
 from pymatgen.core.sites import Site, PeriodicSite
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from spglib import standardize_cell
-from scipy.linalg import expm
 
 
-def make_supercell(structure, min_image_distance, wrap=True, refine=True):
+def rotate(R, lattice_vecs, frac_coords):
+    new_lattice_vecs = np.dot(lattice_vecs.T, R).T
+    new_frac_coords = np.dot(frac_coords, R)
+    return new_lattice_vecs, new_frac_coords
+
+def get_angles(cell_matrix):
+    a, b, c = [v/np.linalg.norm(v) for v in cell_matrix]
+    k_a = np.arccos(np.dot(b, c))
+    k_b = np.arccos(np.dot(a, c))
+    k_c = np.arccos(np.dot(a, b))
+    return k_a, k_b, k_c
+
+
+def make_supercell(structure, min_image_distance, wrap=True, standardize=True,
+        do_niggli_first=True, verbosity=1):
     """
     Creates from a given structure a supercell based on the required minimal dimension
     :param structure: The pymatgen structure to create the supercell for
     :param float min_image_distance: The minimum image distance as a float,
         The cell created will not have any periodic image below this distance
     :param bool wrap: Wrap the atoms into the created cell after replication
-    :param bool refine: Use spglib to refine the created cell, use with care for big structures
+    :param bool standardize: Standardize the created cell.
+        This is done based on the rules in Hinuma etal, http://arxiv.org/abs/1506.01455
+        However, only rules for the triclinic case are applied, so further
+        standardization using spglib is recommended, if a truly standardized
+        cell is required.
+    :param bool do_niggli_first: Start with a niggli reduction of the cell,
+        to enable a faster search. Disable if there are problems with the reduction
+        of if the cell is already Niggli or LLL reduced.
+    :param int verbosity: Sets the verbosity level.
 
     :returns: A new pymatgen core structure instance and the used scaling matrix
+    :returns: The scaling matrix used.
     """
-
     if not isinstance(structure, Structure):
         raise TypeError("Structure passed has to be a pymatgen structure")
     try:
@@ -39,17 +60,30 @@ def make_supercell(structure, min_image_distance, wrap=True, refine=True):
 
     if not isinstance(wrap, bool):
         raise TypeError("wrap has to be a boolean")
-    if not isinstance(refine, bool):
-        raise TypeError("wrap has to be a boolean")
+    if not isinstance(standardize, bool):
+        raise TypeError("standardize has to be a boolean")
 
-    # I'm getting the niggle reduced structure as a first:
-    niggli_reduced = structure.get_reduced_structure(reduction_algo=u'niggli')
+    # I'm getting the niggli reduced structure as first:
+    if verbosity > 1:
+        print "given cell:"
+        print structure._lattice
+    if do_niggli_first:
+        starting_structure = structure.get_reduced_structure(reduction_algo=u'niggli')
+    else:
+        starting_structure = structure
+    if verbosity > 1:
+        print "starting cell:"
+        print starting_structure._lattice
+        for i, v in enumerate(starting_structure._lattice.matrix):
+            print i, np.linalg.norm(v)
+    
     # the lattice of the niggle reduced structure:
-    lattice_cellvecs = niggli_reduced._lattice.matrix
+    lattice_cellvecs = starting_structure._lattice.matrix
     # trial_vecs are all possible vectors sorted by the norm
-    trial_vecs = get_trial_vecs(lattice_cellvecs, min_image_distance)
+    trial_vecs = get_trial_vecs(lattice_cellvecs, min_image_distance, verbosity=verbosity)
     # I pass these trial vectors into the function to find the minimum volume:
-    scale_matrix, supercell_cellvecs = find_min_vol(trial_vecs, lattice_cellvecs, min_image_distance)
+    scale_matrix, supercell_cellvecs = find_min_vol(trial_vecs, lattice_cellvecs, min_image_distance, 
+            verbosity=verbosity)
 
     # Constructing the new lattice:
     new_lattice = Lattice(supercell_cellvecs)
@@ -58,39 +92,117 @@ def make_supercell(structure, min_image_distance, wrap=True, refine=True):
     # and transforrm to cartesian coords here:
     c_lat = new_lattice.get_cartesian_coords(f_lat)
     #~ cellT = supercell_cellvecs.T
-    #~ cellI = np.array(np.matrix(cellT).I)
+
+    if verbosity > 1:
+        print "Given lattice:"
+        print new_lattice
+        for i, v in enumerate(new_lattice.matrix):
+            print i, np.linalg.norm(v)
 
     new_sites = []
-    print "Done, constructing structure"
-    for site in niggli_reduced:
+    if verbosity:
+        print "Done, constructing structure"
+    for site in starting_structure:
         for v in c_lat:
-            pos = site.coords + v
-            if wrap:
-                pos = np.dot(cellT, np.dot(cellI, pos) % 1.0 )
-            s = PeriodicSite(site.species_and_occu, pos,
+            new_sites.append(PeriodicSite(site.species_and_occu, site.coords +v,
                              new_lattice, properties=site.properties,
-                             coords_are_cartesian=True, to_unit_cell=False)
-            new_sites.append(s)
+                             coords_are_cartesian=True, to_unit_cell=wrap))
 
     supercell = Structure.from_sites(new_sites)
-    #~ view(ad.get_atoms(supercell))
-    if refine:
-        from spglib import refine_cell
-        from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-        print "Refining cell using spglib"
-        sa = SpacegroupAnalyzer(supercell, symprec=1e-21, angle_tolerance=-1)
-        oldvolume = supercell._lattice.volume
-        supercell = sa.get_refined_structure()
-        newvolume = supercell._lattice.volume
-        assert abs(oldvolume - newvolume) / newvolume < 1e-10, "Lost volume in refinement {:.5f} {:.5f}".format(oldvolume, newvolume)
+
+    if standardize:
+        frac_coords = np.empty((len(supercell), 3))
+        for i, site in enumerate(supercell):
+            frac_coords[i,:] = site.frac_coords
+
+        # The code below sorts the cell and positions by lattice vector length,
+        # shortest first:
+        veclens = sorted([(np.linalg.norm(v), i) for i, v in enumerate(supercell_cellvecs)])
+        M1 = np.zeros((3,3))
+        for row, (_, idx) in enumerate(veclens):
+            M1[idx, row] = 1
+        supercell_cellvecs, frac_coords = rotate(M1, supercell_cellvecs, frac_coords)
+
+        k_a, k_b, k_c = get_angles(supercell_cellvecs)
+        right_angle = 0.5*np.pi
+        if ((k_a <= right_angle and k_b <= right_angle and k_c <= right_angle ) or 
+            (k_a > right_angle and k_b > right_angle and k_c > right_angle )):
+            M2 = np.eye(3)
+        elif ((k_a <= right_angle and k_b > right_angle and k_c > right_angle ) or 
+            (k_a > right_angle and k_b <= right_angle and k_c <= right_angle )):
+            M2 = np.diag([1,-1,-1])
+        elif ((k_a > right_angle and k_b <= right_angle and k_c > right_angle ) or 
+            (k_a <= right_angle and k_b > right_angle and k_c <= right_angle )):
+            M2 = np.diag([-1,1,-1])
+        elif ((k_a > right_angle and k_b > right_angle and k_c <= right_angle ) or 
+            (k_a <= right_angle and k_b <= right_angle and k_c > right_angle )):
+            M2 = np.diag([-1,-1,1])
+        else:
+            raise RuntimeError("Unrecognized case for k_a={}, k_b={}, k_c={}".format(k_a,k_b, k_c))
+        supercell_cellvecs, frac_coords = rotate(M2, supercell_cellvecs, frac_coords)
+        # Now applying the rules layed out in  http://arxiv.org/abs/1506.01455
+        # to get the standardized triclinic cell.
+        # Since not all cells give to me are triclinic < -> <= with respect to
+        # the paper. So it's not truly standardized!
+        metric = np.dot(supercell_cellvecs, supercell_cellvecs.T)
+
+        a = np.sqrt(metric[0, 0])
+        b = np.sqrt(metric[1, 1])
+        c = np.sqrt(metric[2, 2])
+        alpha = np.arccos(metric[1, 2] / b / c)
+        beta = np.arccos(metric[0][2] / a / c)
+        gamma = np.arccos(metric[0][1] / a / b)
+
+        cg = np.cos(gamma)
+        cb = np.cos(beta)
+        ca = np.cos(alpha)
+        sg = np.sin(gamma)
+
+        supercell_cellvecs = np.zeros((3,3))
+        supercell_cellvecs[0, 0] = a
+        supercell_cellvecs[1, 0] = b * cg
+        supercell_cellvecs[2, 0] = c * cb
+        supercell_cellvecs[1, 1] = b * sg
+        supercell_cellvecs[2, 1] = c * (ca - cb * cg) / sg
+        supercell_cellvecs[2, 2] = c * np.sqrt(1 - ca * ca - cb * cb - cg * cg + 2 * ca * cb * cg) / sg
+        
+        # And some checks:
+        if (
+            supercell_cellvecs[0,0] < -1e-12 or
+            abs(supercell_cellvecs[0,1]) > 1e-12 or
+            abs(supercell_cellvecs[0,2]) > 1e-12):
+            raise ValueError("First lattice vector not aligned with x-axis")
+        if  (
+            supercell_cellvecs[1,1] < -1e-12 or
+            abs(supercell_cellvecs[1,2]) > 1e-12):
+            raise ValueError("Second lattice vector not in X-Y plane in first quadrant")
+        if  (supercell_cellvecs[2,2] < 0):
+            raise ValueError("Third lattice vector not in positive Z-direction")
+        
+        new_sites = []
+        new_lattice = Lattice(supercell_cellvecs)
+
+        for i, site in enumerate(supercell):
+            s = PeriodicSite(site.species_and_occu, frac_coords[i],
+                            new_lattice, properties=site.properties,
+                            coords_are_cartesian=False, to_unit_cell=wrap)
+            new_sites.append(s)
+        
+        supercell = Structure.from_sites(new_sites)
+
+        #~ view(ad.get_atoms(supercell))
+        if verbosity > 1:
+            print "Cell after standardization:"
+            print new_lattice
+            for i, v in enumerate(new_lattice.matrix):
+                print i, np.linalg.norm(v)
+
     return supercell, scale_matrix
-
-
     
 
 
 
-def get_trial_vecs(cell, min_image_distance):
+def get_trial_vecs(cell, min_image_distance, verbosity=1):
     """
     Returns all possible vectors (as integer crystal coordinates)
     that lie outside a sphere of radius min_dist_soll, but within a sphere given by a
@@ -109,7 +221,8 @@ def get_trial_vecs(cell, min_image_distance):
         repetitions[index] = int(math.ceil(min_image_distance/d1))
     maxcell = np.array([rep*v for rep, v in zip(repetitions, cell)])
     diagvol = np.abs(np.dot(np.cross(maxcell[0], maxcell[1]), maxcell[2]))
-    print "Volume of diagonal supercell:", diagvol
+    if verbosity:
+        print "Volume of diagonal supercell:", diagvol
     maxradius =  diagvol /  min_image_distance**2
     # Now I find all vectors that lie within the maxradius but outside the minradius
     # Inversion symmetry allows me to look only in the upper half.
@@ -128,7 +241,7 @@ def get_trial_vecs(cell, min_image_distance):
     trials = sorted(trials)
     return trials
 
-def find_min_vol(trial_vecs, cell, min_image_distance):
+def find_min_vol(trial_vecs, cell, min_image_distance, verbosity=1):
     min_volume = np.inf
     max_min_per_image_distance = 0
     max_radius = trial_vecs[-1][0]
@@ -158,10 +271,8 @@ def find_min_vol(trial_vecs, cell, min_image_distance):
                 d3 = np.abs(np.dot(np.cross(vector1/veclen1, vector2/veclen2), vector3))
                 if d3 < min_image_distance:
                     continue
-                
                 volume = np.abs(np.dot(np.cross(vector1,vector2), vector3))
                 min_per_image_distance = min((d1,d2, d3))
-
                 if volume < min_volume:
                     min_volume = volume
                     max_radius = min_volume/min_image_distance**2
@@ -173,29 +284,12 @@ def find_min_vol(trial_vecs, cell, min_image_distance):
                 else:
                     is_best = False
                 if is_best:
-                    print "New optimal supercell {} & {}".format(volume, max_min_per_image_distance)
+                    if verbosity:
+                        print "New optimal supercell {} & {}".format(volume, max_min_per_image_distance)
                     chosen_coords = np.array(((ia1,ib1,ic1),(ia2,ib2,ic2),(ia3,ib3,ic3)))
                     cellvecs = np.array([vector1, vector2, vector3])
-    print 'FINAL'
-    print chosen_coords
+    if verbosity:
+        print 'FINAL'
+        print chosen_coords
     return np.array(chosen_coords, dtype=int), np.array(cellvecs)
-    
 
-if __name__ == '__main__':
-    from pymatgen.io.cif import CifParser
-    #~ parser = CifParser("LiGePS.cif")
-    parser = CifParser("MyBaseFileNameCollCode421725.cif")
-    structure = parser.get_structures()[0]
-    print 'OLD VOLUME', structure._lattice.volume
-    #~ structure._lattice = Lattice(5*np.eye(3) + np.random.random((3,3)))
-    #~ structure._lattice = Lattice()
-    radius=20
-    make_supercell(structure, radius, wrap=False, refine=True)
-    #~ structure
-    #~ radius = 10
-    #~ cell = np.array(((1, 0.5, -0.1) , (0, 1,-0.2), (00.1,-0.3,1))) 
-    #~ trial_vecs = get_trial_vecs(cell, radius)
-    #~ print "Number of trial vectors:", len(trial_vecs)
-    #for _, i, j, k, _ in trial_vecs:
-    #
-    #~ print i, j, k
